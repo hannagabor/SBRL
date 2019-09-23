c9843503f8d7f7d8d00702e038db52a53b3f0ebf
hannagabor <gahanna999@gmail.com>
2019-09-23 23:59:32 +0200

    2.5 WIP 2.

--- nonstationary-2-5/src/main.rs
+++ nonstationary-2-5/src/main.rs
@@ -1,7 +1,13 @@
-use rand::distributions::{Distribution, Normal};
+use rand::distributions::{Distribution, Normal, Uniform};
+
+const NUM_ARMS: usize = 10;
+const EPSILON: f64 = 0.1;
+const STEPS: i32 = 100;
+
 struct Arm {
     mean: f64,
     variance: f64,
+    change_distribution: Normal,
 }
 
 impl Arm {
@@ -9,6 +15,7 @@ impl Arm {
         Arm {
             mean: 0.,
             variance: 1.,
+            change_distribution: Normal::new(0., 0.01),
         }
     }
 
@@ -17,51 +24,76 @@ impl Arm {
         normal.sample(&mut rand::thread_rng())
     }
 
-    fn change_mean(&mut self) {}
+    fn change_mean(&mut self) {
+        self.mean += self.change_distribution.sample(&mut rand::thread_rng());
+    }
 }
 
 trait Player {
     fn choose(&self) -> usize;
-    fn update_estimations(&mut self, arm: Arm, reward:f64){
-    }
+    fn update_estimation(&mut self, chosen_arm: usize, reward: f64) {}
+}
+
+fn max_index(v: &Vec<f64>) -> usize {
+    1
 }
 
 struct ConstantPlayer {
     estimations: Vec<f64>,
+    alpha: f64,
+    change_distribution: Normal,
+    greedy_distribution: Uniform<f64>,
+}
+
+impl ConstantPlayer {
+    fn new() -> ConstantPlayer {
+        ConstantPlayer {
+            estimations: vec![0.; NUM_ARMS],
+            alpha: 0.1,
+            change_distribution: Normal::new(0., 0.1),
+            greedy_distribution: Uniform::new(0., 1.),
+        }
+    }
 }
 
 impl Player for ConstantPlayer {
     fn choose(&self) -> usize {
+        if self.greedy_distribution.sample(&mut rand::thread_rng()) < EPSILON {
+            Uniform::new(0, NUM_ARMS).sample(&mut rand::thread_rng())
+        } else {
+            max_index(&self.estimations)
+        }
     }
+    fn update_estimation(&mut self, chosen_arm: usize, reward: f64) {}
 }
 
-
-struct AveragePlayer {
-    estimations: Vec<f64>,
-}
-
-impl Player for AveragePlayer {
-    fn choose(&self) -> usize {
-    }
-}
+// struct AveragePlayer {
+//     estimations: Vec<f64>,
+// }
+//
+// impl Player for AveragePlayer {
+//     fn choose(&self) -> usize {
+//     }
+// }
 
 fn main() {
-    const STEPS: i32 = 100;
     let mut arms: Vec<Arm> = vec![];
-    let average_player =  AveragePlayer::new();
+    // let average_player =  AveragePlayer::new();
     let constant_player = ConstantPlayer::new();
-    let players = vec![average_player, constant_player];
-    for _ in 0..10 {
+    // let players = vec![average_player, constant_player];
+    let players = vec![constant_player];
+    for _ in 0..NUM_ARMS {
         let arm = Arm::new();
         arms.push(arm);
     }
-    for player in players {
+    for mut player in players {
         for _ in 0..STEPS {
             for a in &mut arms {
                 a.change_mean();
             }
+            let chosen_arm = player.choose();
+            let reward = arms[chosen_arm].get_reward();
+            player.update_estimation(chosen_arm, reward);
         }
-
     }
-    println!("Hello, world!");
 }
