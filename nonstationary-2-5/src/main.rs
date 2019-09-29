f150f12bfea510b587b7b836568d4be19835acd8
hannagabor <gahanna999@gmail.com>
2019-09-29 18:09:49 +0200

    2.5 WIP.

--- nonstationary-2-5/src/main.rs
+++ nonstationary-2-5/src/main.rs
@@ -1,8 +1,9 @@
 use rand::distributions::{Distribution, Normal, Uniform};
+use gnuplot::{Figure, Caption, Color};
 
 const NUM_ARMS: usize = 10;
 const EPSILON: f64 = 0.1;
-const STEPS: i32 = 100;
+const STEPS: usize = 10000;
 
 struct Arm {
     mean: f64,
@@ -31,18 +32,30 @@ impl Arm {
 
 trait Player {
     fn choose(&self) -> usize;
-    fn update_estimation(&mut self, chosen_arm: usize, reward: f64) {}
+    fn update_inner_state(&mut self, chosen_arm: usize, reward: f64, optimal:bool) {}
 }
 
 fn max_index(v: &Vec<f64>) -> usize {
-    1
+    let mut max_value = 0.;
+    let mut max_index: usize = 0;
+    for (i, &e) in v.iter().enumerate() {
+        if max_value < e {
+            max_index = i;
+            max_value = e;
+        }
+    }
+    max_index
 }
 
 struct ConstantPlayer {
     estimations: Vec<f64>,
     alpha: f64,
-    change_distribution: Normal,
     greedy_distribution: Uniform<f64>,
+    sum_rewards: f64,
+    average_rewards: Vec<f64>,
+    num_optimal_choices: f64, //we want to divide it and get a float
+    optimal_choice_ratio: Vec<f64>,
+    step: usize
 }
 
 impl ConstantPlayer {
@@ -50,8 +63,12 @@ impl ConstantPlayer {
         ConstantPlayer {
             estimations: vec![0.; NUM_ARMS],
             alpha: 0.1,
-            change_distribution: Normal::new(0., 0.1),
             greedy_distribution: Uniform::new(0., 1.),
+            sum_rewards: 0.,
+            average_rewards: vec![0.; STEPS],
+            num_optimal_choices: 0.,
+            optimal_choice_ratio: vec![0.; STEPS],
+            step: 0,
         }
     }
 }
@@ -64,7 +81,17 @@ impl Player for ConstantPlayer {
             max_index(&self.estimations)
         }
     }
-    fn update_estimation(&mut self, chosen_arm: usize, reward: f64) {}
+    fn update_inner_state(&mut self, chosen_arm: usize, reward: f64, optimal: bool) {
+        self.estimations[chosen_arm] += self.alpha * (reward - self.estimations[chosen_arm]);
+        self.sum_rewards += reward;
+        let num_steps = (self.step + 1) as f64;
+        self.average_rewards[self.step] = self.sum_rewards / num_steps;
+        if optimal {
+            self.num_optimal_choices += 1.;
+        }
+        self.optimal_choice_ratio[self.step] = self.num_optimal_choices / num_steps;
+        self.step += 1;
+    }
 }
 
 // struct AveragePlayer {
@@ -81,19 +108,35 @@ fn main() {
     // let average_player =  AveragePlayer::new();
     let constant_player = ConstantPlayer::new();
     // let players = vec![average_player, constant_player];
-    let players = vec![constant_player];
+    let mut players = vec![constant_player];
     for _ in 0..NUM_ARMS {
         let arm = Arm::new();
         arms.push(arm);
     }
-    for mut player in players {
+    for player in &mut players {
         for _ in 0..STEPS {
+            let mut max_mean = std::f64::NEG_INFINITY;
             for a in &mut arms {
                 a.change_mean();
+                max_mean = max_mean.max(a.mean);
             }
-            let chosen_arm = player.choose();
-            let reward = arms[chosen_arm].get_reward();
-            player.update_estimation(chosen_arm, reward);
+            let chosen_arm_index = player.choose();
+            let chosen_arm = &arms[chosen_arm_index];
+            let reward = chosen_arm.get_reward();
+            let optimal = chosen_arm.mean == max_mean;
+            player.update_inner_state(chosen_arm_index, reward, optimal);
         }
     }
+
+    let mut fg = Figure::new();
+    let mut x = vec![];
+    for i in 0..STEPS {
+        x.push(i);
+    }
+    let player = &players[0];
+    println!("{:?}", player.average_rewards);
+    fg.axes2d()
+    .lines(&x, &player.average_rewards, &[Caption("rew"), Color("black")]);
+    fg.show();
+
 }
