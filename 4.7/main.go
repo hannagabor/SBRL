271a0e4b666cde27763ce094acf324ef41fad8cf
Hanna Gabor <gahanna999@gmail.com>
2020-06-15 19:51:48 +0200

    Fix things. Add plot.

--- 4.7/main.go
+++ 4.7/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"gonum.org/v1/gonum/stat/distuv"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
 	"math"
+	"strconv"
 )
 
 const maxCars = 20
 const gamma = 0.9
-const theta = 0.1
+const theta = 0.0001
 
 type State struct {
 	loc1 int
@@ -49,7 +51,7 @@ func (state *State) getActions() []int {
 
 func cutProb(dist *distuv.Poisson, x int, max int) float64 {
 	if x == max {
-		return 1 - dist.CDF(float64(x)-1)
+		return 1 - dist.CDF(float64(x)-0.5)
 	} else {
 		return dist.Prob(float64(x))
 	}
@@ -60,13 +62,14 @@ type StateAction struct {
 	a int
 }
 
-type StateRewardProb struct {
-	s    State
-	r    float64
-	prob float64
+type StateReward struct {
+	s State
+	r float64
 }
 
-type StateRewardProbs []StateRewardProb
+type Prob float64
+
+type StateRewardProbs map[StateReward]Prob
 
 func getP(states []State) map[StateAction]StateRewardProbs {
 	p := make(map[StateAction]StateRewardProbs)
@@ -81,22 +84,27 @@ func getP(states []State) map[StateAction]StateRewardProbs {
 			loc2 := s.loc2 + a
 			reward := -2 * math.Abs(float64(a))
 			for rent1 := 0; rent1 <= loc1; rent1++ {
+				loc1AfterRent := loc1 - rent1
 				rent1Prob := cutProb(rent1Dist, rent1, loc1)
 				for rent2 := 0; rent2 <= loc2; rent2++ {
+					loc2AfterRent := loc2 - rent2
 					rent2Prob := cutProb(rent2Dist, rent2, loc2)
-					for return1 := 0; return1 <= maxCars-loc1; return1++ {
-						return1Prob := cutProb(return1Dist, return1, maxCars-loc1)
-						for return2 := 0; return2 <= maxCars-loc2; return2++ {
-							return2Prob := cutProb(return2Dist, return2, maxCars-loc2)
+					for return1 := 0; return1 <= maxCars-loc1AfterRent; return1++ {
+						return1Prob := cutProb(return1Dist, return1, maxCars-loc1AfterRent)
+						for return2 := 0; return2 <= maxCars-loc2AfterRent; return2++ {
+							return2Prob := cutProb(return2Dist, return2, maxCars-loc2AfterRent)
 							newState := State{
-								loc1 - rent1 + return1,
-								loc2 - rent2 + return2,
+								loc1AfterRent + return1,
+								loc2AfterRent + return2,
 							}
 							r := reward + 10*float64(rent1+rent2)
 							prob := rent1Prob * rent2Prob * return1Prob * return2Prob
 							sa := StateAction{s, a}
-							srp := StateRewardProb{newState, r, prob}
-							p[sa] = append(p[sa], srp)
+							sr := StateReward{newState, r}
+							if p[sa] == nil {
+								p[sa] = make(StateRewardProbs)
+							}
+							p[sa][sr] += Prob(prob)
 						}
 					}
 				}
@@ -107,14 +115,14 @@ func getP(states []State) map[StateAction]StateRewardProbs {
 }
 
 func eval(states []State, p map[StateAction]StateRewardProbs, V []float64, pi []int, theta float64) {
-	for delta := 0.0; delta < theta; {
+	for delta := theta + 1; delta > theta; {
 		delta = 0
 		for _, state := range states {
 			v := V[state.toIndex()]
 			a := pi[state.toIndex()]
 			new := 0.0
-			for _, srp := range (p[StateAction{state, a}]) {
-				new += srp.prob * (srp.r + gamma*V[srp.s.toIndex()])
+			for sr, prob := range (p[StateAction{state, a}]) {
+				new += float64(prob) * (sr.r + gamma*V[sr.s.toIndex()])
 			}
 			V[state.toIndex()] = new
 			possibleDelta := math.Abs(v - new)
@@ -130,18 +138,18 @@ func improve(states []State, p map[StateAction]StateRewardProbs, V []float64, pi
 	for _, state := range states {
 		oldAction := pi[state.toIndex()]
 		oldActionValue := 0.0
-		for _, srp := range (p[StateAction{state, oldAction}]) {
-			oldActionValue += srp.prob * (srp.r + gamma*V[srp.s.toIndex()])
+		for sr, prob := range (p[StateAction{state, oldAction}]) {
+			oldActionValue += float64(prob) * (sr.r + gamma*V[sr.s.toIndex()])
 		}
 		actions := state.getActions()
 		max := oldActionValue
 		argmax := oldAction
 		for _, a := range actions {
 			actionValue := 0.0
-			for _, srp := range (p[StateAction{state, a}]) {
-				actionValue += srp.prob * (srp.r + gamma*V[srp.s.toIndex()])
+			for sr, prob := range (p[StateAction{state, a}]) {
+				actionValue += float64(prob) * (sr.r + gamma*V[sr.s.toIndex()])
 			}
-			if actionValue > max {
+			if actionValue > max || (actionValue == max && math.Abs(float64(a)) < math.Abs(float64(argmax))) {
 				max = actionValue
 				argmax = a
 			}
@@ -154,6 +162,68 @@ func improve(states []State, p map[StateAction]StateRewardProbs, V []float64, pi
 	return policyStable
 }
 
+type Plottable struct {
+	pi []int
+}
+
+func (plottable Plottable) Dims() (c, r int) {
+	c = maxCars + 1
+	r = maxCars + 1
+	return
+}
+
+func (plottable Plottable) Z(c, r int) float64 {
+	s := State{r, c}
+	return float64(plottable.pi[s.toIndex()])
+}
+
+func (plottable Plottable) X(c int) float64 {
+	return float64(c)
+}
+
+func (plottable Plottable) Y(r int) float64 {
+	return float64(r)
+}
+
+func makePlot(states []State, pi []int) {
+	plottable := Plottable{pi}
+	pl, err := plot.New()
+	if err != nil {
+		panic(err)
+	}
+	labels := make([]float64, 0, 11)
+	for i := -5.0; i < 6; i++ {
+		labels = append(labels, i)
+	}
+	contour := plotter.NewContour(plottable, labels, nil)
+	pl.Add(contour)
+	stringLabels := make([]string, 0, 11)
+	for i := -5; i < 6; i++ {
+		stringLabels = append(stringLabels, strconv.Itoa(i))
+	}
+	labelsMap := make(map[int]plotter.XY, 11)
+	for i, state := range states {
+		labelsMap[pi[i]] = plotter.XY{float64(state.loc2), float64(state.loc1)}
+	}
+	XYs := make([]plotter.XY, 11)
+	for i, l := range labels {
+		XYs[i] = labelsMap[int(l)]
+	}
+	ls, err := plotter.NewLabels(plotter.XYLabels{
+		XYs:    XYs,
+		Labels: stringLabels,
+	},
+	)
+	if err != nil {
+		panic(err)
+	}
+	pl.Add(ls)
+	err = pl.Save(1000, 1000, "pi.png")
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	states := getStates()
 	p := getP(states)
@@ -164,6 +234,5 @@ func main() {
 		eval(states, p, V, pi, theta)
 		policyStable = improve(states, p, V, pi)
 	}
-	fmt.Println(V)
-	fmt.Println(pi)
+	makePlot(states, pi)
 }
